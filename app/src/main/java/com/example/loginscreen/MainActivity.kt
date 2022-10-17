dbcd082816c7b96587067f7446e6886f965451e3
Dennis Okumu <dennisokumu72@yahoo.com>
2022-10-17 16:34:30 +0300

    UI and Firebase Connect to android

---
+++ app/src/main/java/com/example/loginscreen/MainActivity.kt
@@ -0,0 +1,220 @@
+package com.example.loginscreen
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.loginscreen.MainActivity.Companion.TAG
+import com.example.loginscreen.ui.theme.LoginScreenTheme
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : ComponentActivity() {
+
+    private val auth by lazy {
+        Firebase.auth
+    }
+    companion object{
+        val TAG : String = MainActivity::class.java.simpleName
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginScreenTheme {
+                // A surface container using the 'background' color from the theme
+                LoginScreen(auth)
+
+            }
+        }
+    }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun LoginScreen(auth: FirebaseAuth) {
+
+
+    val focusManager = LocalFocusManager.current
+    var email by remember{
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    val isEmailValid by derivedStateOf {
+        Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+    val isPasswordValid by derivedStateOf {
+        password.length > 5
+    }
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "Welcome to Login Screen",
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+            fontSize = 28.sp,
+            modifier = Modifier.padding(top  = 16.dp)
+        )
+
+        Image(
+            painterResource(id = R.drawable.bookdoclogo),
+            contentDescription = "Logo",
+            modifier = Modifier.size(150.dp)
+        )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            shape = RoundedCornerShape(16.dp),
+            border = BorderStroke(1.dp, Color.Black)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(all = 10.dp)
+            ) {
+                OutlinedTextField(value = email,
+                    onValueChange = {email = it},
+                    label = { Text(text = "Email Address")},
+                    placeholder = { Text(text = "example@domain.com")},
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    isError = !isEmailValid,
+                    trailingIcon = {
+                        if (email.isNotBlank()){
+                            IconButton(onClick = { email = "" }) {
+                                Icon(imageVector = Icons.Filled.Clear,
+                                    contentDescription = "Clear Email")
+                            }
+                        }
+                    },
+                    keyboardActions = KeyboardActions(
+                        onNext = {focusManager.moveFocus(FocusDirection.Down)}
+                    )
+                )
+
+                OutlinedTextField(value = password,
+                    onValueChange = {password = it},
+                    label = { Text(text = "Password")},
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ),
+                    isError = !isPasswordValid,
+                    keyboardActions = KeyboardActions(
+                        onDone = {focusManager.clearFocus()}
+                    ),
+                    trailingIcon = {
+                        IconButton(onClick = { isPasswordVisible = !isPasswordVisible}) {
+                            Icon(imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                                contentDescription = "Toggle password visibility" )
+                        }
+                    },
+                    visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
+                )
+
+                Button(onClick = {
+                    auth.signInWithEmailAndPassword(email,password)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful){
+                                Log.d(TAG,"User has successfully logged in")
+                            } else {
+                                Log.w(TAG,"User Failed to log in")
+                            }
+                        }
+                },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue),
+                    enabled = isEmailValid && isPasswordValid
+                ) {
+                    Text(text = "Log In",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp,
+                        color = Color.Black
+                    )
+                }
+            }
+        }
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Forgot Password?",
+                    color = Color.Black,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+            }
+        }
+        Button(onClick = { /*TODO*/ },
+            enabled = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+        ) {
+            Text(
+                text = "Register",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginScreenTheme {
+        LoginScreen(Firebase.auth)
+    }
+}
